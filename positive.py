cbc3372986328edfaf202c7507b8fa07ef8ceac4
msaavedra123 <43019009+msaavedra123@users.noreply.github.com>
2018-10-17 21:47:05 -0400

    Update positive.py

---
+++ positive.py
@@ -0,0 +1,12 @@
+def positive(sequence):
+	if type(sequence) != list:
+		raise TypeError("Not a list")	#Will print error message if input is not a list
+	count = 0							#Variable will be used to keep track of positive numbers
+	for i in range(len(sequence)):
+		if sequence[i] > 0:				#If number is greater than 0, will add 1 to count
+			count += 1
+	print count
+
+y = [3, -5, 0, -5, -6, -3, 2, -6, 8, -9, -3, -1, -5]    #3 Positive numbers
+positive(y)
+positive('[-1, -2, 4]') 		#String, should print type error message
